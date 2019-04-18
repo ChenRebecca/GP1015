382315f30077a445f6d46db894ff38dfa459228a
Rebecca <a0963829715@gmail,com>
2019-04-18 18:02:49 +0800

    0418

--- GMT/homework/HW.sh
+++ GMT/homework/HW.sh
@@ -5,19 +5,19 @@ cpt=homework.cpt
  
 gmt set PS_MEDIA A4
  
-gmt makecpt -T0/300/1 -Ccyclic -Z > $cpt
+gmt makecpt -T40/80/1 -Ccyclic -Z > $cpt
 
 # start gmt session
 gmt psxy -R0/1/0/1 -Jm1c -T -K -P > $out_ps 
   
 # Main map
 gmt psbasemap -R119/123/21/26 -Jm3 -B1 -G0/250/250 -X-0.5 -Y5 -O -K >> $out_ps
-gmt psbasemap -R -J -B+tReservoir-temperature -O -K >>$out_ps
+gmt psbasemap -R -J -B+t"Reservoir temperature" -O -K >>$out_ps
 gmt pscoast -R -J -B -W1 -G220/220/220 -Df -Ia -O -K >> $out_ps
 awk '{print $1, $2, $3}' $input_list | gmt psxy -R -J -Sc0.1 -C$cpt -O -K >> $out_ps 
 
-gmt makecpt -Ccyclic -T-40/80 > $cpt
-gmt psscale -Cq.cpt -DjRM+w3i/0.25i+o0.5i/0+mc -R -J -O -K -F+P+i  -Bax5 -By+l"K" >>$out_ps
+
+gmt psscale -C$cpt -DjRM+w3i/0.25i+o0.5i/0+mc -R -J -O -K -F+P+i  -B10 -By+l"K" >>$out_ps
 
 # end gmt session 
 gmt psxy -R -J -O -T >> $out_ps   
